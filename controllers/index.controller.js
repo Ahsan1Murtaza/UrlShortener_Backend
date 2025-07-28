30e26fc907c060b8d9d4f374ee3083f5f635fbdb
Ahsan Murtaza <ahsanmurtaza2005@gmail.com>
2025-07-28 14:51:19 +0500

    Added navigation between login and register pages with basic UI for both

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -5,10 +5,10 @@ const getAllUrlsFromDb = async (req, res)=>{
     const allUrls = await findAllUrlsFromDb(req.user.id)
 
     if (!allUrls.success){
-        return res.render('home', {urls: []})
+        return res.render('home', {urls: [], email: req.user.email})
     }
 
-    return res.render('home', {urls: allUrls.urls})
+    return res.render('home', {urls: allUrls.urls, email: req.user.email})
 }
 
 module.exports = {getAllUrlsFromDb}
